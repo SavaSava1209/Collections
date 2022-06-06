ed67e70e64367d40c54c201f5861ac249d42025e
SavaSava <63306010+SavaSava1209@users.noreply.github.com>
2022-06-06 15:59:10 -0700

    finished infinity scroll page

--- js/infinityScroll.js
+++ js/infinityScroll.js
@@ -1,14 +1,36 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 // const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-const url = `https://api.unsplash.com/photos/random/?client_id=lT9iqjYzUX0M51lftI9hgOsCPtpPDmV2T2I9SzLGhJk&count=10`
+let initialCount = 5
+const apiKey = 'lT9iqjYzUX0M51lftI9hgOsCPtpPDmV2T2I9SzLGhJk'
+let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`
 let photosArray = []
+let ready = false
+let totalImage = 0
+let imagesCount = 0
+let isInitialLoad = true
+
+function imagesLoaded() {
+    imagesCount++    
+    if (totalImage === imagesCount) {
+        ready = true
+        loader.hidden = true
+    }
+}
+
+function updateURLWithNewCount(count) {
+    url = `https://api.unsplash.com/photos/random/?client_id=lT9iqjYzUX0M51lftI9hgOsCPtpPDmV2T2I9SzLGhJk&count=${count}`
+}
 
 async function getPhotos() {
     try{
         const response = await fetch(url)
         photosArray = await response.json()
         displayPhotos()
+        if (isInitialLoad) {
+            updateURLWithNewCount(30)
+            isInitialLoad = false
+        }
         
     } catch(error) {
         console.log(error)
@@ -16,6 +38,8 @@ async function getPhotos() {
 }
 
 function displayPhotos() {
+    imagesCount = 0
+    totalImage = photosArray.length
     photosArray.forEach((photo) => {
         const item = document.createElement('a')
         // item.setAttribute('href', photo.links.html)
@@ -33,7 +57,7 @@ function displayPhotos() {
             'alt': photo.alt_description,
             'title': photo.alt_description
         })
-
+        img.addEventListener('load', imagesLoaded)
         item.appendChild(img)
         imageContainer.appendChild(item)
     })
@@ -47,7 +71,8 @@ function setAttributes(element, attributes) {
 
 // keep loading more photos if scroll to the bottom
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false
         getPhotos()
     }
 })
